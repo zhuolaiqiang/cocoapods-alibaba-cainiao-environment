98caebb0ff9deab7c1bc1640c596c4bf687572a3
亿刀 <laiqiang.zlq@taobao.com>
2019-10-25 22:44:36 +0800

    适配新框架

---
+++ lib/cocoapods_plugin.rb
@@ -0,0 +1,14 @@
+puts 'cainiao plugin of pod run'
+
+require 'cocoapods-alibaba-cainiao-environment/command'
+require 'cocoapods-alibaba-cainiao-environment/environment.rb'
+
+# module CocoapodsGitHooks
+#   Pod::HooksManager.register('cocoapods-cainiao', :post_install) do |context|
+#     # GitHooksSync.new.sync()
+#   end
+#   Pod::HooksManager.register('cocoapods-cainiao', :post_update) do |context|
+#     # GitHooksSync.new.sync()
+#   end
+# end
+
